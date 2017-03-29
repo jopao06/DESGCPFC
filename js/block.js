1bda5c83a8b2e68375bd21b9e810b7274bff35d7
Juan Paulo Bañares <Juan Paulo Baares>
2017-03-30 06:43:56 +0800

    Blocks can be locker per line and edi panel is scrollable

--- js/block.js
+++ js/block.js
@@ -16,20 +16,20 @@ $(document).ready(function(){
   // Drag functions
   var move = function(dx, dy, x, y, e){
     tempElem.attr({
-      transform: "t"+[blockPanel.position().left+dx, blockPanel.position().top+dy]
+      transform: "t"+[blockPanel.offset().left+dx, blockPanel.offset().top+dy]
     });
     tempElem.data('shift',{dx:dx,dy:dy});
-    tempElem.data('mouse', {x:x,y:y})
+    tempElem.data('mouse', {x:x,y:y});
     // console.log(tempElem.data('shift'));
     // console.log(dx+","+dy);
   };
 
   var start = function(){
     $('#overlay').css('visibility', 'visible');
-    // console.log("t"+[this.getBBox().x + blockPanel.position().left, this.getBBox().y + blockPanel.position().top]);
+    // console.log("t"+[this.getBBox().x + compData.blockPanelLeft, this.getBBox().y + compData.blockPanelTop]);
     tempElem = this.clone();
     tempElem.attr({
-      transform: "t"+[blockPanel.position().left, blockPanel.position().top]
+      transform: "t"+[compData.blockPanelLeft, compData.blockPanelTop]
     });
     tempElem.data('origin', { ox:tempElem.getBBox().x, oy: tempElem.getBBox().y });
     tempElem.data('shift',{dx:0,dy:0});
@@ -38,78 +38,223 @@ $(document).ready(function(){
 
   // console.log($(snapEdit.node));
 
+  var dragAllowance = 50;
+  var x, y, gCounter;
+  var lastBlock;
+  var finalRect;
+  var compData = {
+        blockPanelTop : blockPanel.offset().top,
+        blockPanelLeft : blockPanel.offset().left,
+        editPanelTop : editPanel.offset().top,
+        editPanelLeft : editPanel.offset().left,
+
+        blockPanelHeight : blockPanel.height(),
+        editPanelHeight : editPanel.height(),
+        blockPanelWidth : blockPanel.width(),
+        editPanelWidth : editPanel.width(),
+  };
+
+  $( window ).resize(function() {
+    compData.blockPanelTop = blockPanel.offset().top;
+    compData.blockPanelLeft = blockPanel.offset().left;
+    compData.editPanelTop = editPanel.offset().top;
+    compData.editPanelLeft = editPanel.offset().left;
+  });
+
   var end = function(e){
     $('#overlay').css('visibility', 'hidden');
-    // var compData = {
+    // compData = {
     //     blockPanelTop : blockPanel.offset().top,
     //     blockPanelLeft : blockPanel.offset().left,
     //     blockPanelHeight : blockPanel.height(),
-    //     editPanelTop : editPanel.offset().top,
-    //     editPanelLeft : editPanel.offset().left,
+    //     editPanelTop : compData.editPanelTop,
+    //     compData.editPanelLeft : compData.editPanelLeft,
     //     editPanelHeight : editPanel.height(),
     //     x: this.getBBox().x,
     //     y: this.getBBox().y,
     //     dx: tempElem.data('shift').dx,
     //     dy: tempElem.data('shift').dy,
+    //     moX: tempElem.data('mouse').x,
+    //     moY: tempElem.data('mouse').y,
     //     height: this.getBBox().height,
-    //     topDiff: editPanel.offset().top - blockPanel.offset().top,
-    //     leftDiff: editPanel.offset().left - blockPanel.offset().left
+    //     topDiff: compData.editPanelTop - blockPanel.offset().top,
+    //     leftDiff: compData.editPanelLeft - blockPanel.offset().left
     // };
-    // console.log(this.node);
     // console.log(compData);
-    var dragAllowance = 50;
-    var x, y, gCounter;
-    var lastGroup;
-    if( ( tempElem.data('mouse').x + dragAllowance > editPanel.offset().left )    // Check if block is dragged within editor
+    if( ( tempElem.data('mouse').x + dragAllowance > compData.editPanelLeft )                        // Check if block is dragged within editor
         &&
-        ( tempElem.data('mouse').y + dragAllowance > editPanel.offset().top )
+        ( tempElem.data('mouse').y + dragAllowance > compData.editPanelTop )
       )
         {
-          // console.log(editPanel.children('g').length);
           gCounter = editPanel.children('g').length;
-          if(gCounter == 0){
-            x = block_margin;
+          finalElem = this.clone();
+          if(gCounter === 0){                                                                         // Check if there is no block in editor
+            x = blockMargin;
             y = parseInt($(this.node).find("rect").attr('y'));
+
+            // Update Link List
+            compData.prevLine = finalElem;
+            finalElem.node.prevLine = null;
+            finalElem.node.nextLine = null;
+            finalElem.node.right = null;
+            finalElem.node.left = null;
+            compData.head = finalElem;
+            compData.end = finalElem;
           }
           else{
-            lastGroup = editPanel.children('g')[gCounter - 1];
-            x = block_margin;
-            y = lastGroup.getBBox().y + lastGroup.getBBox().height + block_margin;
+            if(tempElem.data('mouse').y > $(compData.end.node).offset().top+compData.end.getBBox().height){ // Check if it is placed after lastblock
+              x = blockMargin;
+              y = compData.end.getBBox().y + compData.end.getBBox().height + blockMargin;
+
+              // Update Link List
+              finalElem.node.prevLine = compData.prevLine;
+              finalElem.node.nextLine = null;
+              compData.end.node.nextLine = finalElem;
+              compData.prevLine = finalElem;
+              compData.end = finalElem;
+            }
+            // If between block
+            else{
+              var compX = tempElem.data('shift').dx - compData.editPanelLeft + compData.blockPanelLeft + this.getBBox().x;
+              var compY = tempElem.data('shift').dy - compData.editPanelTop + compData.blockPanelTop + defaultY;
+              // console.log(compX+", "+compY);
+              var targetBlock = findTarget(compX, compY);
+
+              // If dragged before head
+              if(targetBlock !== null){     // DEBUG: When second block and placed adjaccent to first block, targetblock is null !!!!!!!!!
+                console.log(targetBlock);
+                x = parseInt(targetBlock.node.nextLine.getBBox().x);
+                y = parseInt(targetBlock.node.nextLine.getBBox().y);
+
+                adjustBlocks(targetBlock);
+
+                // Update Link List
+                finalElem.node.prevLine = targetBlock;
+                finalElem.node.nextLine = targetBlock.node.nextLine;
+                targetBlock.node.nextLine.prevLine = finalElem;
+                targetBlock.node.nextLine = finalElem;
+              }
+              else{
+                x = blockMargin;
+                y = defaultY;
+
+                adjustBlocks(targetBlock);
+
+                // Update Link List
+                finalElem.node.prevLine = null;
+                finalElem.node.nextLine = compData.head;
+                compData.head.node.prevLine = finalElem;
+                compData.head = finalElem;
+              }
+
+              // compData.prevLine = finalElem;
+            }
+
+            
           }
 
-          finalElem = this.clone();
-          var finalRect = $(finalElem.node).find("rect");
-          finalRect.attr({
+          finalRect = $(finalElem.node).find("rect");
+          finalRect.attr({            // Update the x and y of the rectangle
             x: x,
             y: y
           })
-          .next().attr({
+          .next().attr({              // Update the x and y of the text
             x: x + textXPadding,
             y: (y - $(this.node).find("rect").attr('y')) + textYPadding
           });
+
+          // console.log(compData);
+          // Bind a hover CSS
+          $(finalElem.node).hover(function(){
+            // console.log($(this));
+            $(this).find('rect').css({"stroke-width":4});
+          }, function(){
+            // console.log("Mouse leave");
+            $(this).find('rect').css({"stroke-width":2});
+          });
+
+          // Add final block to edit panel
           snapEdit.add(finalElem);
+          // console.log(finalElem.node.mx+","+finalElem.node.my);
         }
     tempElem.remove();
+
+    console.log(compData.editPanelTop + compData.editPanelHeight);
+    console.log($(compData.end.node).offset().top+", "+ blockHeight);
+    if ($('svg#edit-panel').find('g').length > 12) {
+      editPanel.attr({
+        height: (blockHeight  + blockMargin) * $('svg#edit-panel').find('g').length + 5
+      });
+    }
   };
 
+  function findTarget(x, y){
+    var target, final;
+    var shortest;
+    var temp;
 
+    if(y > 0){
+      // console.log("Find target");
+      target = compData.head;
+      shortest = Math.abs(target.getBBox().y+target.getBBox().height - y);
+      while(target !== null){
+        // console.log(target);
+        temp = Math.abs(target.getBBox().y+target.getBBox().height - y);
+        // console.log(temp);
+
+        final = temp <= shortest ? target : final;
+        shortest = temp <= shortest ? temp : shortest;
+        // console.log(shortest);
+        target = target.node.nextLine;
+      }
+
+      // console.log(final);
+
+      return final;
+    }
+
+    return null;
+  }
+
+  function adjustBlocks(targetBlock){
+    var ptr = targetBlock !== null ? targetBlock.node.nextLine : compData.head;
+    while(ptr !== null){
+      // console.log(ptr);
+      yAdj = parseInt($(ptr.node).find('rect').attr('y'));
+      ptr.select('rect').animate({            // Update the x and y of the rectangle
+        x: x ,
+        y: yAdj + blockHeight + blockMargin
+      }, 230);
+      ptr.select('text').animate({              // Update the x and y of the text
+        x: x + textXPadding,
+        y: yAdj + blockHeight + textYPadding
+      }, 230);
+
+      ptr = ptr.node.nextLine;
+    }
+  }
+
+  // Block Attributes
   var textXPadding = 15;
-  var textYPadding = 35;
-  var block_margin = 10;
-
-  // Block to clone
-  var defaultX = -41;
+  var textYPadding = 30;
+  var textSize = 15;
+  var blockMargin = 8;
+  var blockHeight = 30;
+  var blockWidth = 20;
+  var defaultX = -25;     // For clone template
   var defaultY =  10;
-  var defaultBlock = snapBlock.rect(defaultX,defaultY,40,40,5);
+
+
+  var defaultBlock = snapBlock.rect(defaultX,defaultY,blockWidth,blockHeight,5);
   defaultBlock.attr({
       'fill-opacity': 0,
       stroke: '#FFFFFF',
       strokeWidth: 2,
       class: "block"
   });
-  var defaultText = snapBlock.text(defaultX+textXPadding,textYPadding,"Q");
+  var defaultText = snapBlock.text(defaultX+textXPadding,textYPadding);
   defaultText.attr({
-    'font-size':20,
+    'font-size': textSize,
     fill: fgWhite
   });
 
@@ -122,7 +267,7 @@ $(document).ready(function(){
     // Clone rectangle block
     tempElem = prevBlock.clone();
     tempElem.attr({
-      x: parseInt(prevBlock.attr('x')) + parseInt(prevBlock.attr('width')) + block_margin,
+      x: parseInt(prevBlock.attr('x')) + parseInt(prevBlock.attr('width')) + blockMargin,
       stroke: blocks[i].color
     });
 
@@ -150,7 +295,8 @@ $(document).ready(function(){
   }
 
   blockPanel.attr({
-    width: parseInt(prevBlock.attr('x')) + parseInt(prevBlock.attr('width')) + 10
+    width: parseInt(prevBlock.attr('x')) + parseInt(prevBlock.attr('width')) + blockMargin,
+    height: blockMargin + blockMargin + blockHeight
   });
 
   $('#scrollable-div').find(".less-equal").addClass('active');
@@ -163,7 +309,4 @@ $(document).ready(function(){
   $('#block-go-right').click(function(){
     $('#scrollable-div').animate({scrollLeft:'+=350px'});
   });
-
-  console.log(snapBlock.select(".repeat").getBBox().x);
-
 });
